4d38f6485632a07fdd1b3e182783acb34f4119b3
matheoxz <matheus.sinto@unesp.br>
2021-07-21 13:11:00 -0300

    Comunicação serial pronta

--- Horta_IoT_Arduino/src/main.cpp
+++ Horta_IoT_Arduino/src/main.cpp
@@ -6,25 +6,33 @@
 #include <ArduinoJson.h>
 
 // Using pin 5 as Rx and pin 6 as Tx
-SoftwareSerial s(5,6);
+SoftwareSerial nodemcu(5,6);
+
+int teste = 0;
 
 void setup() {
   Serial.begin(9600);
   // setup of Serial communication 
-  s.begin(9600);
+  nodemcu.begin(9600);
+  delay(1000);
 }
 
 void loop() {
   //creating the JSON document
   StaticJsonDocument<500> doc;
-  doc["timestamp"]=millis();
+  
 
+  doc["timestamp"]=millis();
+  doc["teste"]=teste;
+  doc["aa"]="aaa";
+  teste++;
   // reading each sensor and storing the values into the JSON document
     //implement
   serializeJsonPretty(doc, Serial);
 
   //send the JSON document to the NodeMCU
-  if(s.available()>0) serializeJson(doc, s);
+  
+  if(nodemcu.available()>0) serializeJson(doc, nodemcu);
 
   delay(5000);
 }
--- Horta_IoT_NodeMCU/src/main.cpp
+++ Horta_IoT_NodeMCU/src/main.cpp
@@ -5,42 +5,45 @@
 #include <ArduinoJson.h>
 #include <MQTT.h>
 #include <string.h>
+#include <SoftwareSerial.h>
 /*********************************************************/
 
-
 /*********************GLOBAL VARIABLES*********************/
+//config files
 StaticJsonDocument<400> doc;
 JsonObject obj;
+
 //Wifi configuration
 char wifiSsid[20];
 char wifiPassword[30];
 
 //MQTT broker and credentials configuration
 char brokerUrl[256];
-char *brokerUsername;
-char *brokerPassword;
+char brokerUsername[20];
+char brokerPassword[20];
 
 //PubSub client
 WiFiClientSecure net;
 MQTTClient client;
 
-
-//millis variable
+//Serial communication, (Rx,Tx)
+SoftwareSerial s(D6, D5);
 unsigned long lastMsg = 0;
 
 /*********************************************************/
 
-
 /************************FUNCTIONS************************/
 
 // load the configuration file data
-bool loadConfig(){
+bool loadConfig()
+{
   //saveConfig();
   //opens the configuration file in read mode
   File configFile = LittleFS.open("/secrets.json", "r");
 
   //check the existence of the configuration file
-  if(!configFile){
+  if (!configFile)
+  {
     Serial.println("failed to open configuration file");
     return false;
   }
@@ -51,9 +54,10 @@ bool loadConfig(){
 
   //Read the file in Json format
   configFile.readBytes(buf.get(), size);
-  
+
   auto error = deserializeJson(doc, buf.get());
-  if(error){
+  if (error)
+  {
     Serial.println("Failed to parse configuration file");
     return false;
   }
@@ -61,35 +65,34 @@ bool loadConfig(){
   obj = doc.as<JsonObject>();
 
   //assign the results to the global variables
-  brokerUsername = (char*)malloc(sizeof(obj["broker_username"]));
-  strcpy(brokerUsername,obj["broker_username"]);
 
-  brokerPassword = (char*)malloc(sizeof(obj["broker_password"]));
-  strcpy(brokerPassword,obj["broker_password"]);
+  strcpy(brokerUsername, obj["broker_username"]);
+  strcpy(brokerPassword, obj["broker_password"]);
 
-  
-  strcpy(wifiSsid,obj["wifi_ssid"]);
-  strcpy(wifiPassword,obj["wifi_password"]);
+  strcpy(wifiSsid, obj["wifi_ssid"]);
+  strcpy(wifiPassword, obj["wifi_password"]);
 
-  strcpy(brokerUrl,obj["broker_url"]);
+  strcpy(brokerUrl, obj["broker_url"]);
 
   return true;
 }
 
 // setup the WiFi and connect to it
-void setupWiFi(){
+void setupWiFi()
+{
   Serial.print("Connecting to ");
   Serial.println(wifiSsid);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifiSsid, wifiPassword);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
-   randomSeed(micros());
+  randomSeed(micros());
 
   Serial.println("");
   Serial.println("WiFi connected");
@@ -97,28 +100,32 @@ void setupWiFi(){
   Serial.println(WiFi.localIP());
 }
 
-//Reconnect to broker 
-void reconnect() {
+//Reconnect to broker
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "Horta-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     net.setInsecure();
-   
-    if (client.connect(clientId.c_str(), brokerUsername, brokerPassword)) {
+
+    if (client.connect(clientId.c_str(), brokerUsername, brokerPassword))
+    {
       Serial.println("connected");
       // Once connected, publish an announcement...
       client.publish("outTopic", "hello world");
       // ... and resubscribe
       client.subscribe("led");
-    } else {
-      
+    }
+    else
+    {
+
       Serial.print("failed, rc=");
       Serial.print(client.lastError());
-     
 
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
@@ -127,37 +134,48 @@ void reconnect() {
   }
 }
 
-void messageReceived(String &topic, String &payload) {
-   Serial.print("Message arrived [");
+void messageReceived(String &topic, String &payload)
+{
+  Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("]: ");
   Serial.print(payload);
 
   // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(LED_BUILTIN, LOW);   
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);  
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 /*********************************************************/
 
 /***************************MAIN**************************/
-void setup() {
+void setup()
+{
   //start serial monitor
   Serial.begin(115200);
 
+  //start serial communication to Arduino board;
+  s.begin(9600);
+
   //mount the file system to access the configuration file
   Serial.println("Mounting file system...");
-  if(!LittleFS.begin()){
+  if (!LittleFS.begin())
+  {
     Serial.println("failed to mount file system");
-    
   }
 
   //load configuration file
-  if (!loadConfig()) {
+  if (!loadConfig())
+  {
     Serial.println("Failed to load configuration file");
-  } else {
+  }
+  else
+  {
     Serial.println("Configuration loaded");
   }
 
@@ -166,24 +184,65 @@ void setup() {
 
   //connect to MQTT broker
   client.begin(brokerUrl, 8883, net);
-  
+
   client.onMessage(messageReceived);
+
+  delay(500);
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
+  if (s.available() > 0)
+  {
+    StaticJsonDocument<1000> msg;
+
+    DeserializationError err = deserializeJson(msg, s);
+
+    if (err == DeserializationError::Ok)
+    {
+      //JsonObject msgObj = msg.as<JsonObject>();
+      client.publish("success", "deserialization ok");
+      const char *aa = msg["aa"];
+      Serial.println(aa);
+      client.publish("outTopic", aa);
+      int teste = msg["teste"];
+      char buffer[33];
+      Serial.println(teste);
+      itoa(teste, buffer, 10);
+      client.publish("outTopic", buffer);
+      long timestamp = msg["timestamp"];
+      Serial.println(timestamp);
+      itoa(timestamp, buffer, 10);
+      client.publish("outTopic", buffer);
+      delay(5000);
+      
+    }
+    else
+    {
+      client.publish("error", "deserialization error");
+      client.publish("error", err.c_str());
+
+      // Flush all bytes in the "link" serial port buffer
+      while (s.available() > 0)
+        s.read();
+    }
+  }
 
   unsigned long now = millis();
-  if (now - lastMsg > 10000) {
+  if (now - lastMsg > 10000)
+  {
     lastMsg = now;
-   
-    Serial.print("Publish message: ");
-    Serial.println("hello world");
-    client.publish("outTopic", "hello world");
+
+    Serial.println("Publish message: debug");
+
+    client.publish("debug", "debug");
   }
 }
 
